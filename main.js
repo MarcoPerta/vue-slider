fdbfecaab4f7e18294e1476091472abaf1e74b96
MarcoPerta <perta.marco@gmail.com>
2022-11-11 14:41:59 +0100

    begin

---
+++ main.js
@@ -0,0 +1,19 @@
+// realizzare uno slider
+// Inserire 2 frecce a sinistra e a destra dell'immagine momentanea dello slider che permettono di andare avanti e indietro tra le immagini
+// BONUS:
+// nello slider ci sono dei pallini in basso al click dei pallini cambia l'immagine nello slider
+// a ogni 3 sec l'immagine cambia automaticamente senza nessun click da parte nostra
+
+
+
+var app = new Vue(
+    {
+      el: '#root',
+      data: {
+      
+     },
+      method: {
+
+     }
+    }
+ );
